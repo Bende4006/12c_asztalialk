d70c44c6c396295912d09757b8ee6c6acb32925b
Bende4006 <szkybend@gmail.com>
2024-09-11 09:27:18 +0200

    aaaa

---
+++ TobbElem/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TobbElem
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("11.-be nem tanította senki a programozást");
+            Console.ReadLine();
+        }
+    }
+}
