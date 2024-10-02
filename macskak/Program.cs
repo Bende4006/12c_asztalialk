de74721edd4b9ecf00d1119800b17ff807e15b26
Bende4006 <szkybend@gmail.com>
2024-10-02 08:13:43 +0200

    enum

---
+++ macskak/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace macskak
+{
+    class Program
+    {
+        enum animals {cat, dog, tiger, wolf };
+        static void Main(string[] args)
+        {
+            
+
+            animals a = animals.cat;
+                animals b = animals.dog;
+                    animals c = animals.tiger;
+                        animals d = animals.wolf;
+
+            if (a==animals.cat)
+            {
+                Console.WriteLine("cat");
+            }
+            if (b==animals.dog)
+            {
+                Console.WriteLine("dog");
+            }
+
+            Console.ReadKey();
+        }
+    }
+}
