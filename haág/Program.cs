9c068aed591974169769735dbfb1cd82593a8d08
Bende4006 <szkybend@gmail.com>
2024-10-02 09:53:57 +0200

    ha

---
+++ haág/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace haág
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            bool b = true;
+            if (true) Console.WriteLine("Ez az igaz ág");
+            else Console.WriteLine("ez a hamis ág");
+
+            b = false;
+            if (b) Console.WriteLine("ಠ_ಠ Ez az igaz ág");
+            else Console.WriteLine("ez a hamis ág");
+
+            Console.ReadKey();
+        }
+    }
+}
